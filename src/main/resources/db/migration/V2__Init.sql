3318ceeb8c79717064b42f729eba67cd6e28bdeb
vijayasanthi-pv <vijayasanthi.pv@gmail.com>
2021-04-24 21:36:35 +0400

    insert changes updated in V2_Init.sql

--- src/main/resources/db/migration/V2__Init.sql
+++ src/main/resources/db/migration/V2__Init.sql
@@ -1,6 +1,6 @@
 SET MODE MySQL;
-insert ignore into robot (id,name,created_at) values (1234567,'robot_1',2021-04-24T12:14:15.139);
-insert ignore into robot (id,name,created_at) values (7777777,'robot_2',2021-04-24T12:14:15.139);
-insert ignore into robot (id,name,created_at) values (9988867,'robot_3',2021-04-24T12:14:15.139);
-insert ignore into robot (id,name,created_at) values (3434344,'robot_4',2021-04-24T12:14:15.139);
-insert ignore into robot (id,name,created_at) values (5658970,'robot_5',2021-04-24T12:14:15.139);
+insert ignore into robot (id,name,created_at) values (1234567,'robot_1','2021-04-24 12:14:15.139');
+insert ignore into robot (id,name,created_at) values (7777777,'robot_2','2021-04-24 12:14:15.139');
+insert ignore into robot (id,name,created_at) values (9988867,'robot_3','2021-04-24 12:14:15.139');
+insert ignore into robot (id,name,created_at) values (3434344,'robot_4','2021-04-24 12:14:15.139');
+insert ignore into robot (id,name,created_at) values (5658970,'robot_5','2021-04-24 12:14:15.139');
