e08af2b7d6385068962a82da07362ba33a9a9f99
vijayasanthi-pv <vijayasanthi.pv@gmail.com>
2021-04-24 10:49:48 +0400

    insert robot with 'created_at' values in V2_Init_sql

--- src/main/resources/db/migration/V2__Init.sql
+++ src/main/resources/db/migration/V2__Init.sql
@@ -1,6 +1,6 @@
 SET MODE MySQL;
-insert ignore into robot (id,name) values (1234567,'robot_1');
-insert ignore into robot (id,name) values (7777777,'robot_2');
-insert ignore into robot (id,name) values (9988867,'robot_3');
-insert ignore into robot (id,name) values (3434344,'robot_4');
-insert ignore into robot (id,name) values (5658970,'robot_5');
+insert ignore into robot (id,name,created_at) values (1234567,'robot_1',2021-04-24T12:14:15.139);
+insert ignore into robot (id,name,created_at) values (7777777,'robot_2',2021-04-24T12:14:15.139);
+insert ignore into robot (id,name,created_at) values (9988867,'robot_3',2021-04-24T12:14:15.139);
+insert ignore into robot (id,name,created_at) values (3434344,'robot_4',2021-04-24T12:14:15.139);
+insert ignore into robot (id,name,created_at) values (5658970,'robot_5',2021-04-24T12:14:15.139);
